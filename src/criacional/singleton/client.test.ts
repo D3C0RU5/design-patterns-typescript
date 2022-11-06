93094592ea5c102b836c645fcbbfbbb8bf97ade5
D3C0RU5 <chpeterle.profissional@gmail.com>
2022-11-06 12:54:14 -0300

    config jest and refactor test files

--- src/criacional/singleton/client.test.ts
+++ src/criacional/singleton/client.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals';
 import { Singleton } from './singleton';
 
 test('Intancias do Singleton deve ser iguais', () => {
-  const s1 = Singleton.getInstance();
-  const s2 = Singleton.getInstance();
-  expect(s1).toBe(s2);
+  const expected = Singleton.getInstance();
+  const result = Singleton.getInstance();
+  expect(expected).toBe(result);
 });
